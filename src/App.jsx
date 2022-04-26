bfe35d98547ccd8c6292409496ef9b2405045e00
DavletArtykov <davletartykov6@gmail.com>
2022-04-26 12:34:44 +0600

    hw8

--- src/App.jsx
+++ src/App.jsx
@@ -10,22 +10,22 @@ const album = [
       avatarUrl: 'http://placekitten.com/g/62/62',
     },
   },
-  // {
-  //   date: new Date(),
-  //   text: 'I hope you enjoy learning React!',
-  //   author: {
-  //     name: 'Hello Kitty',
-  //     avatarUrl: 'http://placekitten.com/g/64/64',
-  //   },
-  // },
-  // {
-  //   date: new Date(),
-  //   text: 'I hope you enjoy learning React!',
-  //   author: {
-  //     name: 'Hello Kitty',
-  //     avatarUrl: 'http://placekitten.com/g/64/64',
-  //   },
-  // },
+  {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'http://placekitten.com/g/64/64',
+    },
+  },
+  {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+      name: 'Hello Kitty',
+      avatarUrl: 'http://placekitten.com/g/64/64',
+    },
+  },
 ];
 
 
